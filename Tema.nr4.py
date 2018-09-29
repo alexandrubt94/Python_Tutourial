0120479e382051026a52c070dc15bebe58b6603e
alexandrubt94 <alexandrubt94@yahoo.com>
2018-09-29 11:08:29 +0300

    fisier 02/29/2018

--- Tema.nr4.py
+++ Tema.nr4.py
@@ -1,10 +1,36 @@
-a = input()
-vector = []
-i = 0
+# def max_value(lst):
+    # assume that 1st elemtn is the biggest
+    # save first element in variable
+
+    # for each elemtn in list
+        # compare each element with the assumed one
+        # if we find a bigger one switch elements
+
+# def min_value(lst):
+    # while int(a) != -1:
+    #     vector.append(int(a))
+    #     a = input()
+    # print(min(vector))
+# a = input()
+# vector = []
+# i = 0
+
+#
+numbers = [8, 3, 12, 5, 654, 5, 12, 90, 12]
+# min = min_value(numbers)
+# # max = max_value(numbers)
+#
+# max_index = len(numbers)
+# print(max_index)
+# print numbers[1:max_index]
+# print numbers[7]
+max_index = len(numbers)
+numbers[4] = 23
+
+print max_index
+print numbers[max_index - 1]
+print numbers[4]
+numbers.insert(0,21)
+print  numbers
 
 
-while int(a) != -1:
-    vector.append(int(a))
-    a = input()
-print(min(vector))
-print(max(vector))
